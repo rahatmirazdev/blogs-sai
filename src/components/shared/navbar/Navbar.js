3d5342ad86a0ea6531004879807d9a29bdaf5fd3
rahatmirazdev <rahatmiraz.dev@gmail.com>
2025-01-24 01:53:58 +0600

    updated the navbar

--- src/components/shared/navbar/Navbar.js
+++ src/components/shared/navbar/Navbar.js
@@ -7,51 +7,53 @@ const Navbar = async () => {
     const user = await getUser();
 
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex="0" role="button" className="btn btn-ghost btn-circle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
+        <div className='container mx-auto'>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex="0" role="button" className="btn btn-ghost btn-circle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h7"
+                                />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex="0"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
                         >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h16M4 18h7"
-                            />
-                        </svg>
+                            <li>
+                                <Link href="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link href="/profile">Profile</Link>
+                            </li>
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex="0"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-                    >
-                        <li>
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/profile">Profile</Link>
-                        </li>
-                    </ul>
                 </div>
-            </div>
-            <div className="navbar-center">
-                <a className="btn btn-ghost text-xl">Blog's</a>
-            </div>
-            <div className="navbar-end">
-                {user ? (
-                    <Link href="/api/auth/logout" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                        Sign Out
-                    </Link>
-                ) : (
-                    <Link href="/api/auth/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Sign In
-                    </Link>
-                )}
+                <div className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Blog's</a>
+                </div>
+                <div className="navbar-end">
+                    {user ? (
+                        <Link href="/api/auth/logout" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                            Sign Out
+                        </Link>
+                    ) : (
+                        <Link href="/api/auth/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Sign In
+                        </Link>
+                    )}
+                </div>
             </div>
         </div>
     );
