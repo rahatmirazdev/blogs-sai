e4d765832be4d9ff2879fc1589069001323dc315
rahatmirazdev <rahatmiraz.dev@gmail.com>
2025-01-24 00:24:24 +0600

    but fixed

--- src/middleware.js
+++ src/middleware.js
@@ -2,13 +2,11 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const { getUser } = await getKindeServerSession();
-  const user = await getUser();
-  const { pathname } = req.nextUrl;
-  if (!user && pathname !== "/api/auth/login") {
-    const url = req.nextUrl.clone();
-    url.pathname = "/api/auth/login";
-    return NextResponse.redirect(url);
+  const { isAuthenticated } = await getKindeServerSession();
+  const isLoggedIn = await isAuthenticated();
+
+  if (!isLoggedIn) {
+    return NextResponse.redirect(new URL('/api/auth/login', req.url));
   }
   return NextResponse.next();
 }
