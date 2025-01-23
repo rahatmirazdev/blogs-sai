7b071d684e76749db8303ea5604f43844b153306
rahatmirazdev <rahatmiraz.dev@gmail.com>
2025-01-23 20:33:33 +0600

    redirection added

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,25 +1,7 @@
-import ProfileInfo from "@/components/shared/profile/ProfileInfo";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
-
-const Profile = async () => {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-
-    if (!user) {
-        redirect("/api/auth/login");
-    }
-
+const Profile = () => {
     return (
         <div className="container mx-auto p-4">
-            <div className="">
-                <h1 className="text-3xl font-bold">Welcome to your profile!</h1>
-                {user ? (
-                    <ProfileInfo />
-                ) : (
-                    <p>No user found, Please login first!</p>
-                )}
-            </div>
+            <h1 className="text-3xl font-bold">Welcome to your profile!</h1>
         </div>
     );
 };
--- src/components/shared/navbar/Navbar.js
+++ src/components/shared/navbar/Navbar.js
@@ -11,19 +11,19 @@ const Navbar = async () => {
                 <div className="flex justify-between items-center">
                     <h1 className="text-3xl font-bold">Blog's</h1>
                     <nav className="flex space-x-4">
-                        <Link href="/">Home</Link>
-                        <Link href="/profile">Profile</Link>
+                        <Link href={"/"}>Home</Link>
+                        <Link href={"/profile"}>Profile</Link>
                     </nav>
                     <div className="flex items-center space-x-4">
                         {user ? (
                             <>
                                 <span>{user.family_name}</span>
-                                <Link href="/api/auth/logout" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sign Out</Link>
+                                <Link href={"api/auth/logout"} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sign Out</Link>
                             </>
                         ) : (
                             <>
-                                <Link href="/api/auth/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign In</Link>
-                                <Link href="/api/auth/register" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Up</Link>
+                                <Link href={"api/auth/login"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign In</Link>
+                                <Link href={"api/auth/register"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Up</Link>
                             </>
                         )}
                     </div>
